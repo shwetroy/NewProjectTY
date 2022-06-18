6f4b6f9e8df70d67836d4e4d3b592302d5d8efdd
USER <USER@LAPTOP-FTOETBD1>
2022-06-18 10:35:44 +0530

    Commit this Project

---
+++ src/test/java/ComcastProject/SampleProject.java
@@ -0,0 +1,12 @@
+package ComcastProject;
+
+public class SampleProject {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Project Started");
+		System.out.println("Project Ended");
+
+	}
+
+}
---
+++ src/test/java/ComcastProject/Test1.java
@@ -0,0 +1,12 @@
+package ComcastProject;
+
+public class Test1 {
+
+	public static void main(String[] args) 
+	{
+		
+System.out.println("jenkins Started");
+System.out.println("jenkins stopped");
+	}
+
+}
